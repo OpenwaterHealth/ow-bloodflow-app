2c108abbb02de8bc9472e7e61a94115ed5768693
George Vigelette <george@openwater.cc>
2025-11-14 11:59:12 -0500

    update release

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
     engine.rootContext().setContextProperty("AppFlags", {
         "advancedSensors": True
     })
-    engine.rootContext().setContextProperty("appVersion", "0.3.8e")
+    engine.rootContext().setContextProperty("appVersion", "0.3.8f")
 
     # Load the QML file
     engine.load(resource_path("main.qml"))
